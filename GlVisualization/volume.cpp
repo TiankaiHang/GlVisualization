dd6cae435819b63581e1ce0ecf56f3eb418b19a4
TiankaiHang <tkhang@seu.edu.cn>
2021-01-02 08:52:58 +0800

    up

--- GlVisualization/volume.cpp
+++ GlVisualization/volume.cpp
@@ -2,7 +2,7 @@
 
 Volume::Volume()
 {
-
+	Init();
 }
 
 Volume::~Volume()
@@ -24,7 +24,7 @@ void Volume::Init()
 	int totalSize = this->volume_d * this->volume_h * this->volume_w;
 	this->_data.resize(totalSize);
 
-	glm::vec3 center(this->volume_w / 2, this->volume_h / 2, this->volume_d / 2);
+	glm::ivec3 center(this->volume_w / 2, this->volume_h / 2, this->volume_d / 2);
 	int radius_ball = 100;
 	int radius_cylinder = 50;
 	int length_cylinder = 2 * int(sqrtf(radius_ball * radius_ball - radius_cylinder * radius_cylinder));
@@ -32,7 +32,13 @@ void Volume::Init()
 	for (int i = 0; i < this->volume_w; ++i) {
 		for (int j = 0; j < this->volume_h; ++j) {
 			for (int k = 0; k < this->volume_d; ++k) {
-
+				bool isInBall = inBall(center.x, center.y, center.z, radius_ball, i, j, k);
+				bool isInCylinder = inCylinder(center.x, center.y, center.z,
+					radius_cylinder, length_cylinder, i, j, k);
+				if (isInBall)
+					this->_data[i * this->volume_d * this->volume_w + j * this->volume_d + k] = 100;
+				if(isInCylinder)
+					this->_data[i * this->volume_d * this->volume_w + j * this->volume_d + k] = 200;
 			}
 		}
 	}
